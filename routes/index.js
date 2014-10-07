4cd7b35a349f1ee3bb1ca02c1f7d234300744a3b
dragonist <erin314@naver.com>
2014-10-07 20:56:41 +0900

    express1

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page.*/
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+
+/* GET hello world */
+router.get('/helloworld',function(req,res){
+	res.render('helloworld',{title:'hello,world!'})
+});
+
+/* GET Userlist page */
+router.get('/userlist',function(req,res){
+	var db = req.db;
+	var collection = db.get('usercollection');
+	collection.find({},{},function(e,docs){
+		res.render('userlist',{
+			"userlist":docs
+		});
+	});
+});
+
+
+module.exports = router;
+
+
